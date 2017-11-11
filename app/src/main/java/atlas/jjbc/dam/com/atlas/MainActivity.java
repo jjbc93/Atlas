dcfe9a66bbd4c1b2aceea4627ce900a8914c28a5
jjbc93 <juanjobernabe93@gmail.com>
2017-11-11 12:28:48 +0100

    Inicio del proyecto

---
+++ app/src/main/java/atlas/jjbc/dam/com/atlas/MainActivity.java
@@ -0,0 +1,13 @@
+package atlas.jjbc.dam.com.atlas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
