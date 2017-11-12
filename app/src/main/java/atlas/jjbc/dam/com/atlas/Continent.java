df6b6db23b572e8166b4bcd59eb87154e028d378
jjbc93 <juanjobernabe93@gmail.com>
2017-11-12 01:01:26 +0100

    selector de paises con validación falta pasar datos a la siguiente vista

--- app/src/main/java/atlas/jjbc/dam/com/atlas/Continent.java
+++ app/src/main/java/atlas/jjbc/dam/com/atlas/Continent.java
@@ -3,21 +3,29 @@ package atlas.jjbc.dam.com.atlas;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Continent extends AppCompatActivity {
     TextView textView;
+    CheckBox countryA, countryB, countryC;
+    String continent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_continent);
         textView = (TextView) findViewById(R.id.continenttName);
+        countryA = (CheckBox) findViewById(R.id.checkBox);
+        countryB = (CheckBox) findViewById(R.id.checkBox2);
+        countryC = (CheckBox) findViewById(R.id.checkBox3);
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
 
         if(bundle != null){
-            String continent = (String) bundle.get("continent");
+            continent = (String) bundle.get("continent");
             textView.setText(continent);
             generateContinent(continent);
         }
@@ -34,18 +42,27 @@ public class Continent extends AppCompatActivity {
                 " euroasiático, situado entre los paralelos 36º y 70º de latitud norte.";
                 textView.setText(description);
                 imageView.setImageResource(R.drawable.europa);
+                countryA.setText("España");
+                countryB.setText("Francia");
+                countryC.setText("Italia");
                 break;
             case "Africa":
                 description = "África es el tercer continente por su extensión, tras Asia y América." +
                 " Está situado entre los océanos Atlántico, al oeste, e Índico, al este";
                 textView.setText(description);
                 imageView.setImageResource(R.drawable.africa);
+                countryA.setText("Kenya");
+                countryB.setText("Uganda");
+                countryC.setText("Somalia");
                 break;
             case "America":
                 description = "América es el segundo continente más grande de la Tierra, después de " +
                 "Asia. Ocupa gran parte del hemisferio occidental del planeta";
                 textView.setText(description);
                 imageView.setImageResource(R.drawable.america);
+                countryA.setText("Ecuador");
+                countryB.setText("Jamaica");
+                countryC.setText("Estados Unidos");
                 break;
             case "Asia":
                 description = "Asia es el continente más extenso y poblado de la Tierra. Con cerca " +
@@ -53,6 +70,9 @@ public class Continent extends AppCompatActivity {
                 "y el 29,45 % de las tierras emergidas y, con 4 140 000 ...";
                 textView.setText(description);
                 imageView.setImageResource(R.drawable.asia);
+                countryA.setText("China");
+                countryB.setText("Japón");
+                countryC.setText("Singapur");
                 break;
             case "Oceania":
                 description = "Oceanía es un continente insular de la Tierra constituido por la plataforma " +
@@ -60,7 +80,23 @@ public class Continent extends AppCompatActivity {
                 "archipiélagos coralinos y volcánicos de Melanesia, Micronesia y Polinesia";
                 textView.setText(description);
                 imageView.setImageResource(R.drawable.oceania);
+                countryA.setText("Australia");
+                countryB.setText("Samoa");
+                countryC.setText("Nueva Zelanda");
                 break;
         }
+    }
+
+    public void countriesSelected(View view){
+        StringBuffer buffer = new StringBuffer();
+        if(!countryA.isChecked() && !countryB.isChecked() && !countryC.isChecked()){
+            Toast.makeText(this, "No se ha seleccionado ningún país", Toast.LENGTH_SHORT).show();
+        }else{
+            buffer.append("PaísA: ").append(countryA.isChecked());
+            buffer.append("PaísB: ").append(countryB.isChecked());
+            buffer.append("PaísC: ").append(countryC.isChecked());
+            Toast.makeText(this, buffer.toString(), Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
